4ed58e358e7945be5507b1daa2c1c2f6d7805934
Raghav Khanna <khanna.ra@northeastern.edu>
2023-12-21 15:16:13 -0500

    modifiers tutorial

--- ToDoApplication/app/src/main/java/com/example/todoapplication/MainActivity.kt
+++ ToDoApplication/app/src/main/java/com/example/todoapplication/MainActivity.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
@@ -22,17 +29,32 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-          Row(
+          Column(
               modifier = Modifier
                   //.fillMaxSize()
-                  .width(300.dp)
+                  //.width(600.dp)
                   //.height(300.dp)
-                  .fillMaxHeight(0.7f)
-                  .background(Color.Green),
-              horizontalArrangement = Arrangement.SpaceAround,
-              verticalAlignment = Alignment.CenterVertically
+                  .fillMaxWidth()
+                  .fillMaxHeight(0.5f)
+                  .background(Color.Green)
+                  //.requiredWidth(300.dp)
+                  //.padding(top = 50.dp)
+                  //.padding(start = 40.dp)
+                  .border(5.dp, Color.Magenta)
+                  .padding(5.dp)
+                  .border(5.dp, Color.Black)
+                  .padding(5.dp)
+                  .border(5.dp, Color.Yellow)
+                  .padding(5.dp)
+
           ) {
-              Text("Hello")
+              Text("Hello", modifier = Modifier.clickable {
+
+
+              })
+                  //, modifier = Modifier
+                  //.offset(0.dp, 20.dp))
+              Spacer(modifier = Modifier.height(50.dp))
               Text("World")
           }
         }

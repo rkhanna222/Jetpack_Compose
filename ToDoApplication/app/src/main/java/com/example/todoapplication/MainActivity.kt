70be44abcc46e3a398e354206c678379185be64f
Raghav Khanna <khanna.ra@northeastern.edu>
2023-12-21 15:58:43 -0500

    ImageCard UI

--- ToDoApplication/app/src/main/java/com/example/todoapplication/MainActivity.kt
+++ ToDoApplication/app/src/main/java/com/example/todoapplication/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.todoapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -18,45 +20,80 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-          Column(
-              modifier = Modifier
-                  //.fillMaxSize()
-                  //.width(600.dp)
-                  //.height(300.dp)
-                  .fillMaxWidth()
-                  .fillMaxHeight(0.5f)
-                  .background(Color.Green)
-                  //.requiredWidth(300.dp)
-                  //.padding(top = 50.dp)
-                  //.padding(start = 40.dp)
-                  .border(5.dp, Color.Magenta)
-                  .padding(5.dp)
-                  .border(5.dp, Color.Black)
-                  .padding(5.dp)
-                  .border(5.dp, Color.Yellow)
-                  .padding(5.dp)
-
-          ) {
-              Text("Hello", modifier = Modifier.clickable {
-
-
-              })
-                  //, modifier = Modifier
-                  //.offset(0.dp, 20.dp))
-              Spacer(modifier = Modifier.height(50.dp))
-              Text("World")
-          }
+            val painter = painterResource(id = R.drawable.snow_child)
+            val description = "Child is playing in the snow"
+            val title = "Child is playing in the snow"
+            Box(modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .padding(16.dp)
+            ) {
+                ImageCard(painter = painter, contentDescription = description, title = title)
+            }
         }
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,contentDescription: String,
+    title:String,
+    modifier: Modifier = Modifier
+){
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(5.dp)
+    ) {
+        Box(modifier = Modifier.height(200.dp)){
+            Image(painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+                )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 200f
+
+                    )
+                )
+            )
+
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ){
+                Text(title, style = TextStyle(color = Color.White, fontSize = 18.sp))
+            }
+        }
+
     }
 }
